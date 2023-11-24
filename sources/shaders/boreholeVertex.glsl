3870038e62b3172ab61c5ff184ea0db6aa5158ff
Alforofus <davidm.22@hotmail.com>
2023-11-24 15:12:24 +0200

    Renamed customLamberShader to boreholeShader.

---
+++ sources/shaders/boreholeVertex.glsl
@@ -0,0 +1,48 @@
+#define LAMBERT
+
+attribute float highlight;
+attribute vec3 instanceColor;
+
+out float vHighlight;
+out vec3 vInstanceColor;
+out vec3 vViewPosition;
+
+#include <common>
+#include <uv_pars_vertex>
+#include <displacementmap_pars_vertex>
+#include <envmap_pars_vertex>
+#include <color_pars_vertex>
+#include <fog_pars_vertex>
+#include <normal_pars_vertex>
+#include <morphtarget_pars_vertex>
+#include <skinning_pars_vertex>
+#include <shadowmap_pars_vertex>
+#include <logdepthbuf_pars_vertex>
+#include <clipping_planes_pars_vertex>
+
+void main()
+{
+	#include <uv_vertex>
+	#include <color_vertex>
+	#include <morphcolor_vertex>
+	#include <beginnormal_vertex>
+	#include <morphnormal_vertex>
+	#include <skinbase_vertex>
+	#include <skinnormal_vertex>
+	#include <defaultnormal_vertex>
+	#include <normal_vertex>
+	vHighlight = highlight;
+	vInstanceColor = instanceColor;
+	#include <begin_vertex>
+	#include <morphtarget_vertex>
+	#include <skinning_vertex>
+	#include <displacementmap_vertex>
+	#include <project_vertex>
+	#include <logdepthbuf_vertex>
+	#include <clipping_planes_vertex>
+	vViewPosition = -mvPosition.xyz;
+	#include <worldpos_vertex>
+	#include <envmap_vertex>
+	#include <shadowmap_vertex>
+	#include <fog_vertex>
+}
